d3f7683307038c71a76abeecb2a22fe5b45eb3de
cleitonBarros <48657479+cleitonBarros@users.noreply.github.com>
2022-05-04 20:17:45 -0300

    commit

--- src/script/main.js
+++ src/script/main.js
@@ -1,16 +1,34 @@
+
+
+
+ 
 // carrossel
 const swiper = new Swiper('.swiper',{
     slidePerView: 1,
+    
+    
+    loop:true,
     pagination:{
         el:'.swiper-pagination'
     },
     mousewheel: true,
     keyboard: true,
+    
+    watchOverflow:false,
+    
     breakpoints:{
-        787:{
+        615:{
             slidesPerView:2,
             setWrapperSize:true,
-
+          
+     
+        },
+        901:{
+            slidesPerView:3,
+            setWrapperSize:true,
+            pagination: false,
+            loop:false,
         }
-    }
+        },
+  
 })
